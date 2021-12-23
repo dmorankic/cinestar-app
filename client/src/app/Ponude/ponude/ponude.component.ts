18d92995047351e7424ee4cf5f7f0152e6873ee4
damirmorankic <damir.morankic@edu.fit.ba>
2021-12-23 14:56:43 +0100

    Prikaz ponuda

--- client/src/app/Ponude/ponude/ponude.component.ts
+++ client/src/app/Ponude/ponude/ponude.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {aplication_settings} from "../../aplication_settings";
 
 @Component({
   selector: 'app-ponude',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ponude.component.scss']
 })
 export class PonudeComponent implements OnInit {
+   ponudePodaci: any;
 
-  constructor() { }
+  constructor(private httpKlijent: HttpClient) {
 
-  ngOnInit(): void {
   }
+  private prikaziPonude() {
+    this.httpKlijent.get(aplication_settings.arminURL + "Ponuda").subscribe(x => {
+      this.ponudePodaci = x;
+    });
+  }
+  ngOnInit(): void {
+    this.prikaziPonude();
+  }
+
+  getPonudePodaci() {
+    if (this.ponudePodaci == null)
+      return [];
+    return this.ponudePodaci;
+  }
+
 
 }
