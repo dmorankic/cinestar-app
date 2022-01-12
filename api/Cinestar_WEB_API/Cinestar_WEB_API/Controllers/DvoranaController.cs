4fe923b184811fb12778acfadb17b7630449e60a
smajla <81872866+RS2SEM@users.noreply.github.com>
2022-01-12 18:05:29 +0100

    kreiranje endpointa za CRUD operacije na dvoranama

---
+++ api/Cinestar_WEB_API/Cinestar_WEB_API/Controllers/DvoranaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Modeli;
+using Servisi.IServisi;
+
+namespace Cinestar_WEB_API.Controllers
+{
+
+    [ApiController]
+    [Route("/cinestar_api/seminarski/[controller]")]
+    public class DvoranaController : BaseController<Dvorana, object>
+    {
+        private readonly IBaseService<Dvorana, object> service;
+
+        public DvoranaController(IBaseService<Dvorana, object> service) : base(service)
+        {
+            this.service = service;
+        }
+
+    }
+}
