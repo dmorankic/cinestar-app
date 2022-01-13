fd33a41bf15726f0f40853d17d348a3e52f2c594
smajla <81872866+RS2SEM@users.noreply.github.com>
2022-01-13 10:40:38 +0100

    kreiranje endpoint za CRUD operacije na ulaznicama
    servis i kontroler

---
+++ api/Cinestar_WEB_API/Cinestar_WEB_API/Controllers/KartaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Modeli;
+using Servisi.IServisi;
+
+namespace Cinestar_WEB_API.Controllers
+{
+    [ApiController]
+    [Route("/cinestar_api/seminarski/[controller]")]
+    public class KartaController : BaseController<Karta, object>
+    {
+        private readonly IBaseService<Karta, object> service;
+
+        public KartaController(IBaseService<Karta, object> service) :base(service)
+        {
+            this.service = service;
+        }
+
+    }
+}
---
+++ api/Cinestar_WEB_API/Servisi/Servisi/KartaService.cs
@@ -0,0 +1,45 @@
+﻿using dataBase;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Modeli;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servisi.Servisi
+{
+    public class KartaService : BaseService<Karta, object>
+    {
+        private readonly IConfiguration config;
+        private readonly ILogger logger;
+        private readonly ApplicationDbContext db_context;
+
+        public KartaService(IConfiguration _config, ILogger _logger, ApplicationDbContext _db_context) : base(_config, _logger, _db_context)
+        {
+            //this.db = db;
+            this.config = _config;
+            this.logger = _logger;
+            db_context = _db_context;
+        }
+
+        public override Karta Update(int id, Karta obj)
+        {
+            var set = db_context.karta;
+
+            var karta = set.Find(id);
+            if (karta != null)
+            {
+                karta.cijena = obj.cijena;
+                karta.projekcijaId=obj.projekcijaId;
+                karta.sjedisteId = obj.sjedisteId;
+
+                return obj;
+            }
+            return null;
+        }
+
+
+    }
+}
