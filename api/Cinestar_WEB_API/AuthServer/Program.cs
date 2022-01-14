f0509eba96754a62560b5235f74ae4624fa15414
smajla <81872866+RS2SEM@users.noreply.github.com>
2022-01-14 17:00:37 +0100

    updejt na .NET 6 auth server

--- api/Cinestar_WEB_API/AuthServer/Program.cs
+++ api/Cinestar_WEB_API/AuthServer/Program.cs
@@ -1,26 +1,53 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AuthServer.Config;
+using Microsoft.OpenApi.Models;
 
-namespace AuthServer
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddIdentityServer()
+              .AddInMemoryClients(IDS_Config.Clients)
+              .AddInMemoryIdentityResources(IDS_Config.IdentityResources)
+              .AddInMemoryApiResources(IDS_Config.ApiResources)
+              .AddInMemoryApiScopes(IDS_Config.ApiScopes)
+              .AddTestUsers(IDS_Config.testUsers)
+              .AddDeveloperSigningCredential();
+
+builder.Services.AddCors(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.AddPolicy(
+      "CorsPolicy",
+      builder => builder.WithOrigins("http://localhost:4200", "http://localhost:44383")
+      .AllowAnyMethod()
+      .AllowAnyHeader()
+      .AllowCredentials());
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AuthServer", Version = "v1" })
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseCors("CorsPolicy");
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AuthServer v1"));
 }
+
+app.UseHttpsRedirection();
+
+app.UseIdentityServer();
+
+app.UseRouting();
+
+app.MapControllers();
+
+app.Run();
