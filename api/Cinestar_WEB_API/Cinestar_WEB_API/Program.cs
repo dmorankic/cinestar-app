712c5e0f299f2e1c5cd448dfb73b6da0933d7e03
smajla <81872866+RS2SEM@users.noreply.github.com>
2022-01-14 17:01:34 +0100

    updejt cinestar_web_api na .NET 6

--- api/Cinestar_WEB_API/Cinestar_WEB_API/Program.cs
+++ api/Cinestar_WEB_API/Cinestar_WEB_API/Program.cs
@@ -1,27 +1,102 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Cinestar_WEB_API;
+using Microsoft.OpenApi.Models;
 
-namespace Cinestar_WEB_API
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication(config =>
 {
-    public class Program
+    config.DefaultScheme = "Cookie";
+    config.DefaultChallengeScheme = "oidc";
+})
+.AddCookie("Cookie")
+.AddOpenIdConnect("oidc", config =>
+{
+    // base-address of identityserver
+    config.Authority = "https://localhost:44376/";
+
+    config.ClientId = "web.client";
+
+    config.ClientSecret = "SuperSecretPassword";
+
+    config.SaveTokens = true;
+
+    config.ResponseType = "code";
+
+});
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "cinestar_api", Version = "v1" });
+    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme."
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
                 {
-
-                    webBuilder.UseStartup<Startup>();
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "bearerAuth"
+                                }
+                            },
+                            new string[] {}
+                    }
                 });
-    }
+});
+
+
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+var emailConfig = builder.Configuration
+.GetSection("EmailConfiguration")
+.Get<Modeli.EmailConfiguration.EmailConfig>();
+
+builder.Services.AddSingleton(emailConfig);
+
+
+//outsourceing services registering
+builder.Services.AddInfrastructure();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cinestar_WEB_API v1"));
 }
+
+app.UseRouting();
+
+app.UseCors();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
