ffa98687a67375d41098d43a242d1b706b21d3fe
smajla <81872866+RS2SEM@users.noreply.github.com>
2022-01-15 18:24:11 +0100

    signal r servis

---
+++ client/src/app/bi-dashboard/Services/signal-r.service.ts
@@ -0,0 +1,60 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import * as signalR from '@microsoft/signalr';
+import { Observable } from 'rxjs';
+import { aplication_settings } from 'src/app/aplication_settings';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignalRService {
+
+  public data:any;
+
+  public connection:signalR.HubConnection;
+
+  constructor(private http:HttpClient) {
+
+  }
+
+  public startConnection = () => {
+
+    this.connection = new signalR.HubConnectionBuilder()
+    .withUrl('https://localhost:44383/dashboard',{skipNegotiation:true,transport:signalR.HttpTransportType.WebSockets})
+    .build();
+
+    this.connection
+    .start()
+    .then(() => console.log('SignalR Connected!'))
+    .catch(err => console.error("error while starting conecction :",err.toString()) );
+
+  }
+
+
+  addServerListener(){
+    this.connection.on("transferchartdata", (data) => {
+      this.data=data;
+    });
+  }
+
+  // getDataObservable():Observable<any[]>{
+  //   return new Observable<any[]>(observer=>{
+  //     this.addTransferChartDataListener();
+  //   })
+  // }
+
+  // public chartDataListener = () => {
+  //   this.connection.on('transferchartdata', (data) => {
+  //     this.data = data;
+  //   });
+  // }
+
+  // private startHttpRequest = () => {
+  //   this.http.get('https://localhost:5001/api/Dashboard')
+  //     .subscribe(res => {
+  //       console.log(res);
+  //     })
+  // }
+
+
+}
