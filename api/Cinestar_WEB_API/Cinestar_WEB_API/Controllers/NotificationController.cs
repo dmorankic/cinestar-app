026013e2094c6c476e805dc36bea49427950a9ef
damirmorankic <damir.morankic@edu.fit.ba>
2022-03-21 15:57:01 +0100

    Upravljanje pretplatama na API-u(pocetna postavka)

---
+++ api/Cinestar_WEB_API/Cinestar_WEB_API/Controllers/NotificationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cinestar_WEB_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+    }
+}
---
+++ api/Modeli/NotificationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modeli
+{
+    public class NotificationModel
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public string Url { get; set; }
+    }
+}
